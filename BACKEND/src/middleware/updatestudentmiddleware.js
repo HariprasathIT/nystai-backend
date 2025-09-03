35069f81f211db7aca89046d0f6a4565f1a677f5
HariprasathIT <hariprasath@adhiba.com>
2025-09-03 12:36:43 +0530

    UPDATE STUDENT VALIDATION

--- BACKEND/src/middleware/updatestudentmiddleware.js
+++ BACKEND/src/middleware/updatestudentmiddleware.js
@@ -10,8 +10,15 @@ export const validateUpdateStudentCourse = [
 
   body('last_name')
     .trim()
-    .isLength({ min: 4, max: 30 }).withMessage('Last name must be between 4 and 30 characters')
-    .isAlpha().withMessage('Last name must contain only letters'),
+    .isLength({ max: 4 }).withMessage("Last name must be at most 4 characters long")
+    .isAlpha().withMessage('Last name must contain only letters')
+    .matches(/^[A-Za-z\s]+$/).withMessage("Last name must contain only in letters"),
+
+  body("last_name")
+    .trim()
+    .notEmpty().withMessage("Last name is required").bail()
+
+  ,
 
   body('dob')
     .trim()
